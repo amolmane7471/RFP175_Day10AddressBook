317a87dc85ff38ea477c5b7a2c9a5fc56b82c5c9
amolmane7471 <maneamol3233@gmail.com>
2022-08-20 12:37:20 +0530

    [Amol]: added address book system UC3 for edit contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -29,4 +29,54 @@ public class AddressBook {
 	{
 	System.out.println(contact.toString());
 	}
+	
+	public void editContact()
+	{
+		 System.out.println("Enter choise to edit :\n 1.firstName \n 2.lastName \n 3.address \n 4.city \n 5.state  \n 6.zip \n 7.phoneNo \n 8.email ");
+	       	int choise = sc.nextInt();
+		    switch (choise) {
+		           	case 1: 
+			            	System.out.println("Enter First Name");
+			                 String firstName = sc.next();
+			                 contact.setFirstName(firstName);
+			                 break;
+			            case 2:
+			                 System.out.println("Enter Last Name");
+			                 String lastName = sc.next();
+			                 contact.setLastName(lastName);
+			                 break;
+			            case 3:
+				             System.out.println("Enter Address");
+			                 String address =  sc.next();
+			                 contact.setAddress(address);
+			                 break;
+			            case 4:
+			                 System.out.println("Enter City");
+			                 String city = sc.next();
+			                 contact.setCity(city);
+			                 break;
+			            case 5:
+			                 System.out.println("Enter State");
+			                 String state = sc.next();
+			                 contact.setState(state);
+			                 break;
+			            case 6:
+			                 System.out.println("Enter ZIP Code");
+			                 String zip = sc.next();
+			                 contact.setZipCode(zip);
+			             	break;
+			            case 7:
+			                 System.out.println("Enter your phone number");
+			                 String phoneNo = sc.next();
+			                 contact.setPhoneNo(phoneNo);
+			                 break;
+			            case 8:
+			                 System.out.println("Enter Email ID");
+			                 String email = sc.next();
+			                 contact.setEmail(email);
+			                 break;
+			           
+		           	}
+
+				}
 }
