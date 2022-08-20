2bd32283e4d3796e159d66741f93407315167b10
amolmane7471 <maneamol3233@gmail.com>
2022-08-20 12:03:35 +0530

    [Amol]: added address book system UC2 for add contact

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,10 +1,28 @@
 package com.bridgelabz.addressbook;
+import java.util.Scanner;
 
 public class AddressBookMain {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome TO Address Book System");
-
+		System.out.println("Welcome TO Address Book System\n");
+		AddressBook addressbook = new AddressBook();
+		int choice; 
+		do
+		{
+			System.out.println("Enter a choice : \n 1.AddContact \n 2.DisplayContact \n 3.Exit");	
+			choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			addressbook.addContact();
+			break;
+		case 2:
+			addressbook.displayContact();
+			break;
+		default :
+			System.out.println("enter valid choice");
+		}
+		}while(choice != 3);
 	}
 
 }
